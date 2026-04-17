7b74f420f4866fad56d5280eb2ea7cfcae63b890
Samoysta <samikap2021@gmail.com>
2026-04-17 13:11:15 +0300

    grass added (i really loved it)

---
+++ Scripts/Plants/Plant1.cs
@@ -0,0 +1,70 @@
+using Godot;
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+public partial class Plant1 : Area2D
+{
+	[Export] Sprite2D plantSprite;
+	[Export] Tween.TransitionType tweener;
+	[Export] float dur;
+	Tween tween;
+	bool isShaking;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		tween?.Kill();
+		tween = CreateTween();
+		tween.SetEase(Tween.EaseType.InOut).SetTrans(tweener);
+		tween.TweenProperty(plantSprite,"skew", Mathf.DegToRad(10), dur).Finished += () => {AnimFinished();};
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		
+
+	}
+
+	void AnimFinished()
+	{
+		int target = 0;
+		if (plantSprite.Skew == Mathf.DegToRad(10))
+		{
+			target = -10;
+		}
+		else
+		{
+			target = 10;
+		}
+		if (!isShaking)
+		{
+			tween?.Kill();
+			tween = CreateTween();
+			tween.SetEase(Tween.EaseType.InOut).SetTrans(tweener);
+			tween.TweenProperty(plantSprite,"skew", Mathf.DegToRad(target), dur).Finished += () => {AnimFinished();};	
+		}
+	}
+
+	void BodyEntered2D(Node2D body)
+	{
+		int dir = 0;
+		if (body is Character)
+		{
+			isShaking = true;
+			CharacterBody2D character = (CharacterBody2D)body;
+			if (character.Velocity.X > 0)
+			{
+				dir = 1;
+			}
+			else if (character.Velocity.X < 0)
+			{
+				dir = -1;
+			}
+			tween?.Kill();
+			tween = CreateTween();
+			tween.SetEase(Tween.EaseType.Out).SetTrans(tweener);
+			tween.TweenProperty(plantSprite,"skew", Mathf.DegToRad(dir * 40), dur / 2).Finished += () => {AnimFinished();};
+			isShaking = false;	
+		}
+	}
+}
