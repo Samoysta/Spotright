e6ef59eeca7a11b232f1784d1bcf02a21e13aec9
Samoysta <samikap2021@gmail.com>
2026-04-22 23:25:24 +0300

    d

--- .godot/mono/temp/obj/Debug/Skiper.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/Skiper.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Skiper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9efb9585d824fd5e1b51dd91453e1468fd6b2bd2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04b02ef7a76c20f4bd2001b2069794c978daebda")]
 [assembly: System.Reflection.AssemblyProductAttribute("Skiper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Skiper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
